4a9e844bb6d95c7fd5a9429da6bef8856b2b222c
Neil Grogan <neil@grogan.ie>
2015-10-27 22:04:34 +0000

    Basic DB working

--- example/__init__.py
+++ example/__init__.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, abort, session
 
 app = Flask(__name__)
-
+from .models import *
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    user = User(request.form['username'], request.form['message'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('message', username=user.username))
+
+@app.route('/message/<username>')
+def message(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('message.html', username=user.username,
+                                           message=user.message)
+
+
--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 from flask.ext.script import Manager, Shell, Server
 from example import app
+from example.models import User
 
 manager = Manager(app)
 manager.add_command("runserver", Server())
 manager.add_command("shell", Shell())
+
+@manager.command
+def createdb():
+    from example.models import db
+    db.create_all()
+
 manager.run()
